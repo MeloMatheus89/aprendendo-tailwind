08808c7a436ba720de2a83aa9305ebb95e44e88b
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-06-06 18:02:14 -0300

    feat: restructure App component and add Container, Main, SearchInput, and Icon components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import Aside from "./components/Aside";
+import { Aside } from "./components/Aside";
+import { Container } from "./components/Container";
+import { Main } from "./components/Main";
+import { SearchInput } from "./components/SearchInput";
 function App() {
   return (
     <div className="bg-neutral-background min-h-screen">
-      <Aside />
+      <Container>
+        <Aside />
+        <Main>
+          <SearchInput name="q" />
+        </Main>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Container/index.jsx
@@ -0,0 +1,3 @@
+export const Container = ({ children }) => {
+  return <div className="max-w-[1200px] m-x-auto flex gap-6 ">{children}</div>;
+};
