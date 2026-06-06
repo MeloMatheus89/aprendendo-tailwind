b53e1f16dd8738262bb5b62b013755913b623302
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-06-06 15:03:20 -0300

    feat: integrate Tailwind CSS and create Aside component with logo
    - Added Tailwind CSS for styling in the project.
    - Created Aside component to display a logo and description.
    - Updated App component to include Aside.
    - Added logo SVG asset for branding.
    - Refactored CSS to use Tailwind's utility classes for layout and design.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Aside from "./components/Aside";
+function App() {
+  return (
+    <div className="bg-neutral-background min-h-screen">
+      <Aside />
+    </div>
+  );
+}
+
+export default App;
